ae9e7f8da84dcb7665057ddfb12102df51869366
Simon Kapl <simon@ahoikapptn.com>
2021-12-29 17:18:02 +0100

    ANDROID-5 - added documentation to library and demo project

--- library/src/main/java/com/itextpdf/android/library/extensions/Context+Extension.kt
+++ library/src/main/java/com/itextpdf/android/library/extensions/Context+Extension.kt
@@ -6,8 +6,15 @@ import com.itextpdf.kernel.pdf.PdfReader
 import com.itextpdf.kernel.pdf.PdfWriter
 import java.io.FileNotFoundException
 
+/**
+ * Returns a pdfDocument object with the given filename in writing mode. The document has no pages when initialized.
+ *
+ * @param fileName  the filename of the pdf that should be written
+ * @param mode      the file creation mode that defines who can access the file. Default is MODE_PRIVATE
+ *                  which only allows the calling application to access the file.
+ * @return  the pdf document in writing mode
+ */
 fun Context.pdfDocumentWriter(fileName: String, mode: Int = Context.MODE_PRIVATE): PdfDocument? {
-
     return try {
         val output = openFileOutput(fileName, mode)
         PdfDocument(PdfWriter(output))
@@ -17,8 +24,13 @@ fun Context.pdfDocumentWriter(fileName: String, mode: Int = Context.MODE_PRIVATE
     }
 }
 
+/**
+ * Returns a pdfDocument object with the given filename in reading mode.
+ *
+ * @param fileName  the filename of the pdf that should be read
+ * @return  the pdf document in reading mode
+ */
 fun Context.pdfDocumentReader(fileName: String): PdfDocument? {
-
     return try {
         val file = getFileStreamPath(fileName).absoluteFile
         PdfDocument(PdfReader(file))
@@ -28,8 +40,15 @@ fun Context.pdfDocumentReader(fileName: String): PdfDocument? {
     }
 }
 
-fun Context.pdfDocument(fileName: String, mode: Int = Context.MODE_PRIVATE): PdfDocument? {
-
+/**
+ * Returns a pdfDocument object with the given filename in stamping mode.
+ *
+ * @param fileName  the filename of the pdf that should be written
+ * @param mode      the file creation mode that defines who can access the file. Default is MODE_PRIVATE
+ *                  which only allows the calling application to access the file.
+ * @return  the pdf document in stamping mode
+ */
+fun Context.pdfDocumentStamping(fileName: String, mode: Int = Context.MODE_PRIVATE): PdfDocument? {
     return try {
         val readFile = getFileStreamPath(fileName).absoluteFile
         val output = openFileOutput(fileName, mode)
