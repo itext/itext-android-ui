b7d90404b5b1e35e9b01735c412ef54cb585d43a
Simon Kapl <simon@ahoikapptn.com>
2022-02-03 14:52:15 +0100

    ANDROID-13 - cleanup in tests

--- library/src/androidTest/java/com/itextpdf/android/library/ContextExtensionInstrumentedTest.kt
+++ library/src/androidTest/java/com/itextpdf/android/library/ContextExtensionInstrumentedTest.kt
@@ -1,20 +1,14 @@
 package com.itextpdf.android.library
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import androidx.test.core.app.launchActivity
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.core.content.FileProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.itextpdf.android.library.extensions.getFileName
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import java.io.File
-import java.lang.Exception
-import androidx.core.content.FileProvider
-import com.itextpdf.android.library.test.BuildConfig.APPLICATION_ID
 import java.util.*
 
 /**
@@ -46,7 +40,8 @@ class ContextExtensionInstrumentedTest {
         // use fileProvider to get file uri that starts with "content://"
         val uri = FileProvider.getUriForFile(
             Objects.requireNonNull(appContext),
-            appContext.packageName + ".provider", file);
+            appContext.packageName + ".provider", file
+        )
         fileName = appContext.getFileName(uri)
         assertEquals(expectedFileName, fileName)
     }
